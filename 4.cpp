553d9c1941c3376ed5977f74eeebaa7eb7e6ed67
Polina Lukina <lukina.pv@yandex.ru>
2023-09-07 10:30:03 +0300

    1 commit

---
+++ 4.cpp
@@ -0,0 +1,13 @@
+#include <locale.h>
+#include "stdio.h"
+int main()
+{
+    setlocale(LC_ALL,"RUS");
+    puts("      |\\           ");
+    puts("      | \\         ");
+    puts("      |  \\         ");
+    puts("   -----------     ");
+    puts("   \\________/      ");
+    getchar(); 
+
+}
